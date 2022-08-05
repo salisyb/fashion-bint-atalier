a636722e58853e21347a76fb256e52c9d2f380b7
SalisYB <salisbell404@gmail.com>
2022-08-05 23:47:54 +0100

    added a config

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,12 @@
 import { create } from "apisauce";
 
 // define the api
+// export const api = create({
+//   baseURL: "https://api-bint-atelier.herokuapp.com",
+//   headers: { Accept: "application/json" },
+// });
+
 export const api = create({
-  baseURL: "https://api-bint-atelier.herokuapp.com",
+  baseURL: "http://192.168.0.124:8000",
   headers: { Accept: "application/json" },
 });
