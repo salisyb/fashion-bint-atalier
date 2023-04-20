9a73d789c00f0f508d31acd818875b8fc6c80427
Salisu Abubakar <69687430+salisyb@users.noreply.github.com>
2023-04-20 11:29:06 -0700

    Update config.js

--- src/api/config.js
+++ src/api/config.js
@@ -2,7 +2,7 @@ import { create } from "apisauce";
 
 // define the api
  export const api = create({
-   baseURL: "https://api-bint-atelier.herokuapp.com",
+   baseURL: "https://apit-bint-atelier-production.up.railway.app",
    headers: { Accept: "application/json" },
  });
 
